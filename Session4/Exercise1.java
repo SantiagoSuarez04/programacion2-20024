22619707de5bd6a740459895ebf570d495db448f
zapato <santiago.suarez39880@ucaldas.edu.co>
2024-03-08 10:59:26 -0500

    cambios main

--- Session4/Exercise1.java
+++ Session4/Exercise1.java
@@ -1,13 +1,8 @@
 package Session4;
 public class Exercise1 {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-
-        int suma = sumaElementosArray(array);
-
-        System.out.println("La suma de los elementos del array es: " + suma);
-    }
-    public static int sumaElementosArray(int[] array) {
+        static int[] array = {1, 2, 3, 4, 5};
+        
+    public static int sumaElementosArray() {
         int suma = 0;
         for (int i = 0; i < array.length; i++) {
             suma += array[i];
--- Session4/main.java
+++ Session4/main.java
@@ -1,5 +1,7 @@
 package Session4;
-
-public class main {
-    
+public class Main {
+    static Exercise1 instance_Exercise1= new Exercise1();
+    public static void main(String[] args) {
+       System.out.println("la suma de los elementos del array es: "+Exercise1.sumaElementosArray());
+    }
 }
